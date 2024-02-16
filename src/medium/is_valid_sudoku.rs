dea33acda8cff0374be7f8357f0467ed152567c6
Taufik Pragusga <taufik@pragusga.com>
2024-02-16 21:33:45 +0700

    add doc

--- src/medium/is_valid_sudoku.rs
+++ src/medium/is_valid_sudoku.rs
@@ -5,28 +5,33 @@ use std::collections::{HashMap, HashSet};
 
 impl Solution {
     pub fn is_valid_sudoku(board: Vec<Vec<char>>) -> bool {
+        // HashMap to store sets of numbers for each row, column, and 3x3 box
         let mut map: HashMap<String, HashSet<char>> = HashMap::new();
+
+        // Iterate through each cell in the board
         for (i, row) in board.into_iter().enumerate() {
             for (j, ch) in row.into_iter().enumerate() {
+                // Skip empty cells
                 if ch == '.' {
                     continue;
                 }
 
-                let bi = ((i / 3) as f32).floor() as u8;
-                let bj = ((j / 3) as f32).floor() as u8;
+                // Calculate the index of the 3x3 box containing the cell
+                let bi = (i / 3) as u8;
+                let bj = (j / 3) as u8;
 
+                // Generate keys for the box, row, and column
                 let key_box = format!("B_{}{}", bi, bj);
                 let key_row = format!("R_{}", i);
                 let key_col = format!("C_{}", j);
 
+                // Check if the current number violates the Sudoku rules in the box
                 let set_box = map.get_mut(&key_box);
-
                 match set_box {
                     Some(set_box) => {
                         if set_box.contains(&ch) {
                             return false;
                         }
-
                         set_box.insert(ch);
                     }
                     None => {
@@ -36,14 +41,13 @@ impl Solution {
                     }
                 }
 
+                // Check if the current number violates the Sudoku rules in the row
                 let set_row = map.get_mut(&key_row);
-
                 match set_row {
                     Some(set_row) => {
                         if set_row.contains(&ch) {
                             return false;
                         }
-
                         set_row.insert(ch);
                     }
                     None => {
@@ -53,13 +57,13 @@ impl Solution {
                     }
                 }
 
+                // Check if the current number violates the Sudoku rules in the column
                 let set_col = map.get_mut(&key_col);
                 match set_col {
                     Some(set_col) => {
                         if set_col.contains(&ch) {
                             return false;
                         }
-
                         set_col.insert(ch);
                     }
                     None => {
